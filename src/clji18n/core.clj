d677785f789e1b0650c8c27a91fee3ac63c25792
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-11-08 00:37:01 -0200

    made some functions private

--- src/clji18n/core.clj
+++ src/clji18n/core.clj
@@ -21,19 +21,19 @@
 
 (def empty-tree {})
 
-(def empty-locale (Locale. nil nil nil))
+(def #^{:private true} empty-locale (Locale. nil nil nil))
 
 (defn add-bundle
   ([tree locale bundle] (assoc tree locale bundle))
   ([tree bundle] (assoc tree empty-locale bundle)))
 
-(defn locale-seq [{:keys [language country variant]}]
+(defn- locale-seq [{:keys [language country variant]}]
   (distinct [(locale language country variant)
              (locale language country)
              (locale language)
              empty-locale]))
 
-(defn bundle-seq [tree locale]
+(defn- bundle-seq [tree locale]
   (keep tree (locale-seq locale)))
 
 (defn has-resource? [tree locale key]
@@ -44,13 +44,13 @@
     #(and (has-key? % key) (get-resource % key))
     (bundle-seq tree locale)))
 
-(defn java-locale [{:keys (language country variant)}]
+(defn- java-locale [{:keys (language country variant)}]
   (java.util.Locale. language (or country "") (or variant "")))
 
 (defn internationalize [tree locale key & args]
   (let [pattern (resource tree locale key)
         mf (doto (MessageFormat. "")
-             ;for some reason it won't work creating the MessageFormat with the
+             ;for some reason it won't work if I create the MessageFormat with the
              ;pattern, maybe I need to assign the locale before the pattern
              (.setLocale (java-locale locale))
              (.applyPattern pattern))]
@@ -77,3 +77,4 @@
     (reduce #(apply add-bundle %1 %2)
             tree
             (partition 2 locale-tree-pairs))))
+
